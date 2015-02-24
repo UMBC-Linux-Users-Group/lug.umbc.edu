fc8f7900d2a024ab1b3dc17b4460fe8f23bdb272
Alexander Bauer <sasha@linux.com>
2015-02-23 23:50:20 -0500

    Add more apparently scaffolding code for sidenav

--- style.js
+++ style.js
@@ -1,8 +1,14 @@
 function navbarOn() {
-    alert("Here");
     $("body").addClass("mui-overlay-on");
+    $("#mui-overlay").show();
+}
+
+function navbarOn() {
+    $("body").removeClass("mui-overlay-on");
+    $("#mui-overlay").hide();
 }
 
 $(function() {
+    $("#mui-overlay").hide();
     $("#appbar-sidenav-toggle").click(navbarOn);
 });
