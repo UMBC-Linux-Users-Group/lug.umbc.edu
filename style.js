3498b4563b8663c6a10d214db045683935f77771
Alexander Bauer <sasha@linux.com>
2015-02-24 13:41:20 -0500

    Implement primitive sidenav

--- style.js
+++ style.js
@@ -1,14 +1,34 @@
+// Enable the side navbar.
 function navbarOn() {
     $("body").addClass("mui-overlay-on");
     $("#mui-overlay").show();
 }
 
-function navbarOn() {
+// Disable the side navbar.
+function navbarOff() {
     $("body").removeClass("mui-overlay-on");
     $("#mui-overlay").hide();
 }
 
 $(function() {
+    // Hide the side navbar by default.
     $("#mui-overlay").hide();
-    $("#appbar-sidenav-toggle").click(navbarOn);
+
+    // Clicking anywhere in the document will close the navbar, except on the navbar itself.
+    $(document).click(function() {
+      navbarOff();
+    })
+    $("#sidenav").click(function(e) {
+      // Don't send the click event to the document if it landed on the navbar.
+      e.stopPropagation();
+      return false;
+    })
+
+    // Bind the menu button to opening the navbar.
+    $("#appbar-sidenav-toggle").click(function(e) {
+      // Don't send the click onward past the button.
+      navbarOn();
+      e.stopPropagation();
+      return false;
+    });
 });
