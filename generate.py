2536e651d2920a13efb45320479d34a590a49488
Alexander Bauer <sasha@linux.com>
2015-02-25 18:22:07 -0500

    Recreate material demo with static templates

---
+++ generate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import os, configparser
+
+try:
+    import jinja2 as jinja
+except ImportError:
+    print("To compile this website, you must install jinja")
+    os.exit(1)
+
+cp = configparser.ConfigParser()
+cp.optionxform = str # be case sensitive in keys
+cp.read('demo.conf')
+conf_main = cp['main']
+
+env = jinja.Environment(loader=jinja.FileSystemLoader(conf_main.get('TemplatePath')))
+
+print(env.get_template('muipage-titlepanel.html').render(pages = cp['pages']))
